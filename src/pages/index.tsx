37b458496d068bbc698a42a9ba077f032fb80440
João Marcos Magalhães Madureira <joaommmadureira@hotmail.com>
2021-04-20 23:54:56 -0300

    first steps on the aplication development

---
+++ src/pages/index.tsx
@@ -0,0 +1,12 @@
+import Head from 'next/head';
+
+export default function Home() {
+  return (
+    <div>
+      <Head>
+        <title>ig.news</title>
+      </Head>
+      <h1>Hello World!</h1>
+    </div>
+  )
+}
